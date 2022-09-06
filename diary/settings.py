3733ca9cdba764642e4367f3e2792966ed8a4d4d
ugis70194 <ugis70194@yahoo.co.jp>
2022-09-06 14:23:52 +0900

    ref #issue/7 secret keyをlocal_settingから読み込むように変更

--- diary/settings.py
+++ diary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .local_setting import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v!01(k2(5o7!2tdlz2bz(*d5*znn9njgk%_n5n$8wacyq^ktid'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
